375e7fde8c3c18f45c0c4d9ea49eaf98a5442302
NickTk <nikolaytkachenko1@gmail.com>
2020-12-17 19:30:53 +0200

    removed tests folder

--- gcp_functions/tests/http_test.py
+++
@@ -1,8 +0,0 @@
-from unittest.mock import Mock
-
-import main
-
-def test_cosine():
-    data = {'id': 540992, 'cnd': [19769, 538131, 540992, 446072, 540018]}
-    req = Mock(get_json = Mock(return_value = data), args = data)
-    assert len(main.get_similar_goods(req) > 0)
